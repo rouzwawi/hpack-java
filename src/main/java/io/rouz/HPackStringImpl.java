682f59266de480242c522af55f055dd23b5a0824
Rouzbeh Delavari <rouzwawi@gmail.com>
2015-03-01 17:09:32 +0100

    define huffman code statically

--- src/main/java/io/rouz/HPackStringImpl.java
+++ src/main/java/io/rouz/HPackStringImpl.java
@@ -1,19 +1,8 @@
 package io.rouz;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 final class HPackStringImpl implements HuffmanString {
 
-  private static final String HUFFMAN_CODE_FILE = "huffman.code";
-
-  private static final byte Oxff = (byte) 0xff;
-  private static final byte[] EOS = new byte[] {Oxff, Oxff, Oxff, 0x3f};
-
   private final HNode CODE_TREE = readCode();
-  private final byte[][] CODE_WORDS = new byte[256][];
 
   private final VarInt varInt;
 
@@ -51,7 +40,7 @@ final class HPackStringImpl implements HuffmanString {
       node = b ? node.c1 : node.c0;
 
       if (node.leaf()) {
-        sb.append(node.codepoint);
+        sb.append(node.symbol);
         node = CODE_TREE;
         depth = 0;
         allOnes = true;
@@ -76,80 +65,55 @@ final class HPackStringImpl implements HuffmanString {
   }
 
   private static HNode readCode() {
-    final ClassLoader classLoader = HPackStringImpl.class.getClassLoader();
-    final InputStream codeStream = classLoader.getResourceAsStream(HUFFMAN_CODE_FILE);
-    final BufferedReader in = new BufferedReader(new InputStreamReader(codeStream));
+    final int[] codes = HuffmanCode.CODES;
+    final byte[] codeLengths = HuffmanCode.CODE_LENGTHS;
 
     HNode node = null;
 
-    int lineno = 0;
-    try {
-      char codepoint = 0;
-      String line = null;
+    for (int i = 0; i < codes.length; i++) {
+      int code = codes[i];
+      int codeLen = codeLengths[i];
 
-      while ((line = in.readLine()) != null) {
-        lineno++;
-        line = line.trim();
-        if (line.isEmpty()) continue;
-        if (line.charAt(0) == '#') continue;
-
-        node = insert(node, codepoint, line);
-        codepoint++;
-      }
-    } catch (IOException e) {
-      throw new RuntimeException("Failed to load " + HUFFMAN_CODE_FILE, e);
-    } catch (IllegalArgumentException e) {
-      throw new RuntimeException("Error parsing huffman file on line " + lineno, e);
+      node = insert(node, (char) i, code, codeLen, 0);
     }
 
     return node;
   }
 
-  private static HNode insert(HNode node, char codepoint, String line)
-      throws IllegalArgumentException {
-    return insert(node, codepoint, line, 0);
-  }
-
-  private static HNode insert(HNode node, char codepoint, String line, int pos)
-      throws IllegalArgumentException {
-    if (pos == line.length()) {
-      return new HNode(codepoint);
+  private static HNode insert(HNode node, char symbol, int code, int len, int pos) {
+    if (pos == len) {
+      return new HNode(symbol);
     }
 
-    final char b = line.charAt(pos);
-    if (b != '0' && b != '1') {
-      throw new IllegalArgumentException("Invalid character '" + b + "'");
-    }
+    final boolean b = (code & (1 << (len - 1 - pos))) != 0;
 
     final HNode branch =
         node != null
-            ? b == '0' ? node.c0 : node.c1
+            ? b ? node.c1 : node.c0
             : null;
-    final HNode insert = insert(branch, codepoint, line, pos + 1);
+    final HNode insert = insert(branch, symbol, code, len, pos + 1);
 
-    if (b == '0') {
-      return new HNode(insert, node != null ? node.c1 : null);
-    } else {
+    if (b) {
       return new HNode(node != null ? node.c0 : null, insert);
+    } else {
+      return new HNode(insert, node != null ? node.c1 : null);
     }
   }
 
-  public static void call() {
-  }
-
+  // TODO: re-implement as prefix table
   private static final class HNode {
     final HNode c0, c1;
-    final char codepoint;
+    final char symbol;
 
     private HNode(HNode c0, HNode c1) {
       this.c0 = c0;
       this.c1 = c1;
-      this.codepoint = 0;
+      this.symbol = 0;
     }
 
-    private HNode(char codepoint) {
+    private HNode(char symbol) {
       this.c0 = this.c1 = null;
-      this.codepoint = codepoint;
+      this.symbol = symbol;
     }
 
     boolean leaf() {
